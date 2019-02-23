395258fbaaeae4f9604bb0c7231ab41e196dd689
s00172641 <s00172641@mail.itsligo.ie>
2019-02-23 14:12:11 +0000

    Added CRUD operations to service POST,UPDATE,DELETE with error handling

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -10,13 +10,29 @@ import { Student } from 'src/app/interfaces/student';
 export class StudentComponent implements OnInit {
 
  public students: Student[] = [];
+ addStudent: Student = {
+   FirstName: 'John',
+   LastName: 'Parker',
+   StudentID: 5,
+   RegistrationDate: new Date()
+ }
+ updateStudent: Student;
+
   constructor( private studentService: StudentService) { }
 
   ngOnInit() {
+    this.studentService.addStudent(this.addStudent);
     this.studentService.getAllStudents().subscribe(data => {
      this.students = data;
      console.log(data)
     });
+    this.studentService.getStudentByID(5).subscribe(data => {
+      this.updateStudent = data
+      this.updateStudent.FirstName = 'Johny';
+      console.log(data)
+    });
+    this.studentService.updateStudent(5, this.updateStudent);
+    this.studentService.deleteStudent(5);
   }
 
 }
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,18 +1,64 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http'
 import { Student } from '../interfaces/student';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
 import { HttpClientModule } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = "http://localhost:8080/api";
+
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
-
-  private api: string = 'http://localhost:8080/api'
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
   getAllStudents(): Observable<Student[]> {
-    return this.http.get<Student[]>(this.api + '/Students')
+    return this.http.get<Student[]>(apiUrl + '/Students').pipe(
+      tap(students => console.log('fetched students')),
+      catchError(this.handleError('getAllStudents', []))
+    );
   }
 
+  getStudentByID(id: number): Observable<Student> {
+    const url = `${apiUrl}/${id}`
+    return this.http.get<Student>(url).pipe(
+      tap(_ => console.log(`fetched student id=${id}`)),
+      catchError(this.handleError<Student>(`getStudentByID id=${id}`))
+    );
+  }
+
+  addStudent(student: any): Observable<Student> {
+    return this.http.post<Student>(apiUrl, student, httpOptions).pipe(
+      tap((studentRes: Student) => console.log(`added Student w/ id=${studentRes.StudentID}`)),
+      catchError(this.handleError<Student>('addStudent'))
+    );
+  }
+
+  updateStudent(id: number, student: any): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.put(url, student, httpOptions).pipe(
+      tap(_ => console.log(`updated Student id=${id}`)),
+      catchError(this.handleError<any>('updateStudent'))
+    );
+  }
+
+  deleteStudent(id: number): Observable<Student> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.delete<Student>(url, httpOptions).pipe(
+      tap(_ => console.log(`deleted Student id=${id}`)),
+      catchError(this.handleError<Student>(`deleteStudent`))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+
+      return of(result as T);
+    };
+  }
 }
