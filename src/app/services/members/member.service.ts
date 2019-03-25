8f100a721649fcad8b5179d8bc3ad261c049476a
Gary <s00171995@mail.itsligo.ie>
2019-03-25 22:36:59 +0000

    fixed again

--- src/app/services/members/member.service.ts
+++ src/app/services/members/member.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Member } from 'src/app/interfaces/Member';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -11,10 +9,10 @@ export class MemberService {
   API: string = "http://localhost:57229/api/Members"
   constructor(private http: HttpClient) { }
 
-  searchMembers(membername: string): Observable<Member[]>{
+  searchMembers(membername: string): Observable<any[]>{
     const httpOptions = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "http://localhost:4200" }),
       };
-  return  this.http.get<Member[]>(this.API + "/getMembers/" + membername, httpOptions)
+  return  this.http.get<any[]>(this.API + "/getMembers/" + membername, httpOptions)
   }
 }
